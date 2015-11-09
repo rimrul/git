e7b9ff8f69c91f936eff62796e16c7e2923e7964
Matthias Aßhauer <mha1993@live.de>
2015-11-09 18:32:09 +0100

    stash--helper: implement "git stash--helper"
    This patch implements a new "git stash--helper" builtin plumbing
    command that will be used to migrate "git-stash.sh" to C.
    
    We start by implementing only the "--non-patch" option that will
    handle the core part of the non-patch stashing.
    
    Signed-off-by: Matthias Aßhauer <mha1993@live.de>

---
+++ stash.h
@@ -0,0 +1,10 @@
+#ifndef STASH_H
+#define STASH_H
+
+#include "git-compat-util.h"
+#include "gettext.h"
+#include "run-command.h"
+
+extern int stash_non_patch(const char*, const char*, const char*);
+
+#endif // STASH_H
