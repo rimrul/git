a57c1aea84f7f3e4952b98d11d1c2f5c0e60238f
Johannes Schindelin <johannes.schindelin@gmx.de>
2016-01-18 14:18:35 +0100

    fixup! stash--helper: implement "git stash--helper"
    Repeat upstream's `!strcmp()` idiom.

--- builtin/stash--helper.c
+++ builtin/stash--helper.c
@@ -7,7 +7,7 @@ static const char builtin_stash__helper_usage[] = {
 
 int cmd_stash__helper(int argc, const char **argv, const char *prefix)
 {
-	if (argc == 4 && strcmp("--non-patch", argv[1]) == 0)
+	if (argc == 4 && !strcmp("--non-patch", argv[1]))
 		return stash_non_patch(argv[2], argv[3], prefix);
 	else
 		usage(builtin_stash__helper_usage);
